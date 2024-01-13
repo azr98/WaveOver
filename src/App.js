2ec4e6507e6dab7a570c61e5874e2b8beec1d24d
azr98 <azhar981@gmail.com>
2024-01-13 19:21:07 +0000

    index, app and signup added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import SignUp from './components/Auth/SignUp';
+import Login from './components/Auth/Login';
+import Verification from './components/Auth/Verification';
+import Dashboard from './components/Dashboard/Dashboard';
+import Timer from './components/Timer/Timer';
+import EmailForm from './components/EmailForm/EmailForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/signup" component={SignUp} />
+        <Route path="/login" component={Login} />
+        <Route path="/verify" component={Verification} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/timer" component={Timer} />
+        <Route path="/email-form" component={EmailForm} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SignUp() {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      {/* Form for signing up will go here */}
+    </div>
+  );
+}
+
+export default SignUp;
