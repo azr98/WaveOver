09d763a1deb4b649da485d6b8e39c283f0c534a4
azr98 <azhar981@gmail.com>
2024-01-17 21:12:13 +0000

    send email using celery task in route

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import boto3
 from botocore.exceptions import ClientError
 import datetime
+from celery_tasks import send_email_reminder
 app = Flask(__name__)
 
 
@@ -9,22 +10,6 @@ app = Flask(__name__)
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-def send_email(recipient, subject, body):
-    ses_client = boto3.client('ses', region_name='eu-west-1')
-    try:
-        response = ses_client.send_email(
-            Destination={'ToAddresses': [recipient]},
-            Message={
-                'Body': {'Text': {'Data': body}},
-                'Subject': {'Data': subject},
-            },
-            Source='azhar981@gmail.com'
-        )
-    except ClientError as e:
-        print(e.response['Error']['Message'])
-        return None
-    return response
 
 @app.route('/')
 def index():
@@ -40,3 +25,19 @@ def submit_form():
     # Logic to store this data in your database
     # Schedule email reminders based on start_time
     return jsonify({'status': 'Success', 'message': 'Form submitted and emails scheduled'}), 200
+
+@app.route('/timer-completion', methods=['POST'])
+def timer_completion():
+    data = request.json
+    user_email = data['user_email']
+    spouse_email = data['spouse_email']
+
+    # Retrieve the messages from the database
+    user_message = get_user_message(user_email)
+    spouse_message = get_spouse_message(spouse_email)
+
+    # Send the final emails
+    send_email_reminder.delay(user_email, "Message from your spouse", spouse_message)
+    send_email_reminder.delay(spouse_email, "Message from you", user_message)
+
+    return {'message': 'Messages sent to both parties'}, 200
--- server/email_service.py
+++ server/email_service.py
@@ -2,5 +2,17 @@ import boto3
 from botocore.exceptions import ClientError
 
 def send_email(recipient, subject, body):
-    # SES email sending logic
-    # ...
+    ses_client = boto3.client('ses', region_name='eu-west-1')
+    try:
+        response = ses_client.send_email(
+            Destination={'ToAddresses': [recipient]},
+            Message={
+                'Body': {'Text': {'Data': body}},
+                'Subject': {'Data': subject},
+            },
+            Source='azhar981@gmail.com'
+        )
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        return None
+    return response
