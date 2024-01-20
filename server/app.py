777b81f914c2e44d38bef3331c453bb18feee7fe
azr98 <azhar981@gmail.com>
2024-01-20 15:37:08 +0000

    fixed put_item dynamo data dict

--- server/app.py
+++ server/app.py
@@ -2,7 +2,8 @@ from flask import Flask, request, jsonify
 import boto3
 from botocore.exceptions import ClientError
 import datetime
-from flask_cors import CORS
+from flask_cors import CORS , cross_origin
+
 from celery_tasks import send_email_reminder
 app = Flask(__name__)
 CORS(app)
@@ -11,16 +12,17 @@ CORS(app)
 if __name__ == '__main__':
     app.run(debug=True)
 
-def store_form_data(user_email, spouse_email, issue_headline, start_time):
+@cross_origin()
+def store_form_data(table_data):
     dynamodb = boto3.resource('dynamodb', region_name='eu-west-1')
     table = dynamodb.Table('WaveOver')
     try:
         response = table.put_item(
            Item={
-                'user_email': user_email,
-                'spouse_email': spouse_email,
-                'issue_headline': issue_headline,
-                'start_time': start_time.strftime("%Y-%m-%d %H:%M:%S")  # Format datetime as a string
+                'user_email':table_data['user_email'],
+                'spouse_email': table_data['spouse_email'],
+                'issue_headline': table_data[issue_headline],
+                'start_time': table_data[start_time.strftime("%Y-%m-%d %H:%M:%S")]  # Format datetime as a string
             }
         )
         return response
@@ -33,14 +35,21 @@ def index():
     return "WaveOver Backend Server"
 
 @app.route('/submit_form', methods=['POST'])
+@cross_origin()
 def submit_form():
     data = request.json
-    user_email = data['user_email']
-    spouse_email = data['spouse_email']
-    start_time = datetime.datetime.now()
-    issue_headline = data['issue_headline']
+    response = {
+        'user_email': data['user_email'],
+        'spouse_email': data['spouse_email'],
+        'start_time': datetime.datetime.now(),
+        'issue_headline': data['issue_headline']
+    }
+    # user_email = data['user_email']
+    # spouse_email = data['spouse_email']
+    # start_time = datetime.datetime.now()
+    # issue_headline = data['issue_headline']
     # Storing data in DynamoDB
-    store_form_data(user_email, spouse_email, issue_headline, start_time)
+    store_form_data(response)
     # Schedule email reminders based on start_time
     #TODO connect this to front end dashboard button and test dynamo put upload
     return jsonify({'status': 'Success', 'message': 'Form submitted and emails scheduled'}), 200
@@ -48,6 +57,7 @@ def submit_form():
 
 
 @app.route('/timer-completion', methods=['POST'])
+@cross_origin()
 def timer_completion():
     data = request.json
     user_email = data['user_email']
