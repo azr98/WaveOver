5f811cc4064e24db34ee92b01c70132123f9a754
azr98 <azhar981@gmail.com>
2025-02-05 17:27:25 +0000

    using server cleanup.sh file

---
+++ server/server-cleanup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+REPOSITORY="waveover-development-backend"
+AWS_REGION="eu-west-1"
+REGISTRY="058264329805.dkr.ecr.$AWS_REGION.amazonaws.com"
+
+echo "Stopping and removing the old container (if exists)..."
+docker stop waveover-container || true
+docker rm waveover-container || true
+
+echo "Fetching latest image ID from ECR..."
+LATEST_IMAGE_ID=$(docker images --format "{{.ID}}" $REGISTRY/$REPOSITORY:latest)
+
+echo "Finding all old images except the latest..."
+docker images $REGISTRY/$REPOSITORY --format "{{.ID}} {{.Repository}}:{{.Tag}}" | grep -v $LATEST_IMAGE_ID | awk '{print $1}' | xargs -r docker rmi -f
+
+echo "Pruning unused resources..."
+docker system prune -f
