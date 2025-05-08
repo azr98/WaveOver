f5572391e35903af1c3c40ef8e294f3962785c4d
azr98 <azhar981@gmail.com>
2025-05-08 15:30:06 +0100

    help page added

---
+++ src/app/help/page.js
@@ -0,0 +1,15 @@
+"use client";
+// Next.js help page, refactored from src/components/helpPage.js
+import Header from "../../components/Header";
+import "../../components/css/helpPage.css";
+
+export default function HelpPage() {
+  return (
+    <div className="help-container">
+      <Header />
+      <div className="help-content">
+        {/* Content will be added later */}
+      </div>
+    </div>
+  );
+} 
