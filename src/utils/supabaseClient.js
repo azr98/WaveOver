569465a987f55308eb779a015a42fdfd27da25d4
azr98 <azhar981@gmail.com>
2025-05-24 12:46:31 +0100

    added supabase client

---
+++ src/utils/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseServiceRoleKey); 
