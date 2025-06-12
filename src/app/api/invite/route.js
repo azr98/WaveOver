bfecb49a61dda94a4f608b464d24eecc4ac6df3e
azr98 <azhar981@gmail.com>
2025-06-12 16:49:06 +0100

    updated invite email

--- src/app/api/invite/route.js
+++ src/app/api/invite/route.js
@@ -21,7 +21,7 @@ export async function POST(req) {
     // Expecting: user_email, user_firstname, user_lastname, spouse_email, spouse_firstname, spouse_lastname, argument_topic, submission_time
 
     // Compose email
-    const email_subject = `${data.user_firstname} ${data.user_lastname} wants to discuss '${data.argument_topic}' with ${data.spouse_firstname} ${data.spouse_lastname}`;
+    const email_subject = `${data.user_firstname} ${data.user_lastname} wants to discuss '${data.argument_topic}' with you`;
     const email_body_html = `
       <!DOCTYPE html>
       <html>
@@ -29,12 +29,11 @@ export async function POST(req) {
       <title>${email_subject}</title>
       </head>
       <body>
-      <p>${data.user_firstname} ${data.user_lastname} wants to discuss '${data.argument_topic}' with ${data.spouse_firstname} ${data.spouse_lastname}.</p>
-      <p>Once ${data.spouse_firstname} signs up or logs in at <a href="dev.waveover.info">dev.waveover.info</a> and accepts the discussion, a 3 day timer will start.</p>
+      <p>Just sign up or log in at <a href="dev.waveover.info">dev.waveover.info</a> and accept the discussion in 'Pending'. Then a 3 day timer will start.</p>
       <p>Both of you have all that time to write your say in the text editor. No more, no less.</p>
       <p>Open the text editor by clicking 'Display Current Discussions' -> 'Active' -> and click the discussion.</p>
       <p>After 3 days what each of you wrote is sent to the other by email automatically.</p>
-      <p>Both of ${data.user_firstname} and ${data.spouse_firstname} will receive a reminder 2 days, 1 day, 12 hours and 4 hours before the 3 day deadline.</p>
+      <p>Both of you will receive a reminder 2 days, 1 day, 12 hours and 4 hours before the 3 day deadline.</p>
       <p>For a more detailed guide read here: <a href="https://waveover.info/help">https://waveover.info/help</a></p>
       <p>Thanks for using my app!,</p>
       <p>Azhar, creator of WaveOver</p>
@@ -42,7 +41,7 @@ export async function POST(req) {
       </html>
     `;
 
-    const addresses = [data.user_email, data.spouse_email];
+    const addresses = [data.spouse_email];
     await sendEmail(addresses, email_subject, email_body_html);
 
     // Update last_email_sent in Supabase
